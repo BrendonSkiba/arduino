c5143611c812b77c12091af0ca44941cd0f6126a
BrendonSkiba <brendonskiba@gmail.com>
2017-04-20 10:15:33 -0700

    Add files via upload

---
+++ Functions_bas.ino
@@ -0,0 +1,280 @@
+// POLICE LIGHTS V1
+// RGBW (Red Green Blue White Neo-Pixel starter code
+// 16 LEDS  increment (Functions)
+// Brendon A. Skiba -17.04.20 
+int clearColor();
+int one();
+int two();
+int three();
+int yellow();
+int white();
+int yellow1();
+int white1();
+int yellow2();
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 6
+
+#define NUM_LEDS 16
+
+#define BRIGHTNESS 50
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
+
+
+void setup() {
+  Serial.begin(115200);
+  strip.setBrightness(BRIGHTNESS);
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+// Initialize some variables for the void loop()
+
+
+void loop() {
+  clearColor();
+  one();
+  two();
+  three();
+  yellow();
+  yellow1();
+  yellow2();
+  white();
+  white1();
+}
+
+int clearColor(){
+  int red = 0, green= 0, blue = 255, white = 0;
+int wait = 250;
+int led = 0;
+int i;
+  for ( led = 0; led < 16; led++){  
+    strip.setPixelColor(led, red, green , blue, white);
+  }//end of for loop
+    strip.show();
+    delay(wait);
+    red = red + 8;
+    green = green + 16;
+    blue = blue + 32;
+    white = white + 1;
+
+    if (red > 255) red = 0;
+    if (green > 255) green = 0;
+    if (blue > 255) blue = 0;
+    if (white > 50) white = 0;
+       
+    if (red < 0) red = 255;
+    if (green  < 0) green = 255;
+    if (blue < 0) blue = 255;
+    if (white  < 0 ) white = 50;
+  }
+int one(){
+  int red = 255, green= 0, blue = 0, white = 0;
+int wait = 250;
+int led = 0;
+int i;
+  for ( led = 0; led < 16; led++){  
+    strip.setPixelColor(led, red, green , blue, white);
+  }//end of for loop
+    strip.show();
+    delay(wait);
+    red = red + 8;
+    green = green + 16;
+    blue = blue + 32;
+    white = white + 1;
+
+    if (red > 255) red = 0;
+    if (green > 255) green = 0;
+    if (blue > 255) blue = 0;
+    if (white > 50) white = 0;
+       
+    if (red < 0) red = 255;
+    if (green  < 0) green = 255;
+    if (blue < 0) blue = 255;
+    if (white  < 0 ) white = 50;
+  }
+
+int two(){
+  int red = 0, green= 0, blue = 255, white = 0;
+int wait = 250;
+int led = 0;
+int i;
+  for ( led = 0; led < 16; led++){  
+    strip.setPixelColor(led, red, green , blue, white);
+  }//end of for loop
+    strip.show();
+    delay(wait);
+    red = red + 8;
+    green = green + 16;
+    blue = blue + 32;
+    white = white + 1;
+
+    if (red > 255) red = 0;
+    if (green > 255) green = 0;
+    if (blue > 255) blue = 0;
+    if (white > 50) white = 0;
+       
+    if (red < 0) red = 255;
+    if (green  < 0) green = 255;
+    if (blue < 0) blue = 255;
+    if (white  < 0 ) white = 50;
+  }
+int three(){
+  int red = 255, green= 0, blue = 0, white = 0;
+int wait = 250;
+int led = 0;
+int i;
+  for ( led = 0; led < 16; led++){  
+    strip.setPixelColor(led, red, green , blue, white);
+  }//end of for loop
+    strip.show();
+    delay(wait);
+    red = red + 8;
+    green = green + 16;
+    blue = blue + 32;
+    white = white + 1;
+
+    if (red > 255) red = 0;
+    if (green > 255) green = 0;
+    if (blue > 255) blue = 0;
+    if (white > 50) white = 0;
+       
+    if (red < 0) red = 255;
+    if (green  < 0) green = 255;
+    if (blue < 0) blue = 255;
+    if (white  < 0 ) white = 50;
+  }
+int yellow(){
+  int red = 255, green= 255, blue = 0, white = 0;
+int wait = 100;
+int led = 0;
+int i;
+  for ( led = 0; led < 16; led++){  
+    strip.setPixelColor(led, red, green , blue, white);
+  }//end of for loop
+    strip.show();
+    delay(wait);
+    red = red + 8;
+    green = green + 16;
+    blue = blue + 32;
+    white = white + 1;
+
+    if (red > 255) red = 0;
+    if (green > 255) green = 0;
+    if (blue > 255) blue = 0;
+    if (white > 50) white = 0;
+       
+    if (red < 0) red = 255;
+    if (green  < 0) green = 255;
+    if (blue < 0) blue = 255;
+    if (white  < 0 ) white = 50;
+  }
+
+int white(){
+  int red = 255, green= 255, blue = 255, white = 255;
+int wait = 100;
+int led = 0;
+int i;
+  for ( led = 0; led < 16; led++){  
+    strip.setPixelColor(led, red, green , blue, white);
+  }//end of for loop
+    strip.show();
+    delay(wait);
+    red = red + 8;
+    green = green + 16;
+    blue = blue + 32;
+    white = white + 1;
+
+    if (red > 255) red = 0;
+    if (green > 255) green = 0;
+    if (blue > 255) blue = 0;
+    if (white > 50) white = 0;
+       
+    if (red < 0) red = 255;
+    if (green  < 0) green = 255;
+    if (blue < 0) blue = 255;
+    if (white  < 0 ) white = 50;
+  }
+
+int yellow1(){
+  int red = 255, green= 255, blue = 0, white = 0;
+int wait = 100;
+int led = 0;
+int i;
+  for ( led = 0; led < 16; led++){  
+    strip.setPixelColor(led, red, green , blue, white);
+  }//end of for loop
+    strip.show();
+    delay(wait);
+    red = red + 8;
+    green = green + 16;
+    blue = blue + 32;
+    white = white + 1;
+
+    if (red > 255) red = 0;
+    if (green > 255) green = 0;
+    if (blue > 255) blue = 0;
+    if (white > 50) white = 0;
+       
+    if (red < 0) red = 255;
+    if (green  < 0) green = 255;
+    if (blue < 0) blue = 255;
+    if (white  < 0 ) white = 50;
+  }
+
+int white1(){
+  int red = 255, green= 255, blue = 255, white = 255;
+int wait = 100;
+int led = 0;
+int i;
+  for ( led = 0; led < 16; led++){  
+    strip.setPixelColor(led, red, green , blue, white);
+  }//end of for loop
+    strip.show();
+    delay(wait);
+    red = red + 8;
+    green = green + 16;
+    blue = blue + 32;
+    white = white + 1;
+
+    if (red > 255) red = 0;
+    if (green > 255) green = 0;
+    if (blue > 255) blue = 0;
+    if (white > 50) white = 0;
+       
+    if (red < 0) red = 255;
+    if (green  < 0) green = 255;
+    if (blue < 0) blue = 255;
+    if (white  < 0 ) white = 50;
+  }
+
+int yellow2(){
+  int red = 255, green= 255, blue = 0, white = 0;
+int wait = 100;
+int led = 0;
+int i;
+  for ( led = 0; led < 16; led++){  
+    strip.setPixelColor(led, red, green , blue, white);
+  }//end of for loop
+    strip.show();
+    delay(wait);
+    red = red + 8;
+    green = green + 16;
+    blue = blue + 32;
+    white = white + 1;
+
+    if (red > 255) red = 0;
+    if (green > 255) green = 0;
+    if (blue > 255) blue = 0;
+    if (white > 50) white = 0;
+       
+    if (red < 0) red = 255;
+    if (green  < 0) green = 255;
+    if (blue < 0) blue = 255;
+    if (white  < 0 ) white = 50;
+  }
+
